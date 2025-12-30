f6bab6c20a14801abd46cbd03abc20fdf1fa9429
Aman Mishra <thegreatestaman999@gmail.com>
2025-12-30 10:11:40 +0530

    Fix MongoDB database name to matka

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const Admin = require("../models/Admin");
 const SubAdmin = require("../models/SubAdmin"); // ✅ FIX HERE
