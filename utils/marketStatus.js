aad02ce56e6b660a6728f1440ad22fb182d958c7
Aman Mishra <thegreatestaman999@gmail.com>
2025-12-30 15:03:35 +0530

    a

--- utils/marketStatus.js
+++ utils/marketStatus.js
@@ -1,18 +1,18 @@
 module.exports = function getStatus(openTime, closeTime) {
-  if (!openTime || !closeTime) {
-    return "CLOSED"; // safety fallback
-  }
+  if (!openTime || !closeTime) return "CLOSED";
+
+  const now = new Date();
+  const currentMinutes = now.getHours() * 60 + now.getMinutes();
 
   const [oh, om] = openTime.split(":").map(Number);
   const [ch, cm] = closeTime.split(":").map(Number);
 
-  const now = new Date();
-  const nowMinutes = now.getHours() * 60 + now.getMinutes();
-
   const openMinutes = oh * 60 + om;
   const closeMinutes = ch * 60 + cm;
 
-  return nowMinutes >= openMinutes && nowMinutes < closeMinutes
-    ? "OPEN"
-    : "CLOSED";
+  if (currentMinutes >= openMinutes && currentMinutes < closeMinutes) {
+    return "OPEN";
+  }
+
+  return "CLOSED";
 };
