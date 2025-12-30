0098cffe8a1bce8bd5ca34624a513dd26367b47a
Aman Mishra <thegreatestaman999@gmail.com>
2025-12-30 15:33:49 +0530

    git push

--- utils/marketStatus.js
+++ utils/marketStatus.js
@@ -1,18 +1,16 @@
-module.exports = function getStatus(openTime, closeTime) {
+function getStatus(openTime, closeTime) {
   if (!openTime || !closeTime) return "CLOSED";
 
   const now = new Date();
-  const currentMinutes = now.getHours() * 60 + now.getMinutes();
 
-  const [oh, om] = openTime.split(":").map(Number);
-  const [ch, cm] = closeTime.split(":").map(Number);
+  // IST conversion
+  const ist = new Date(now.getTime() + 5.5 * 60 * 60 * 1000);
+  const current = ist.toTimeString().slice(0,5);
 
-  const openMinutes = oh * 60 + om;
-  const closeMinutes = ch * 60 + cm;
-
-  if (currentMinutes >= openMinutes && currentMinutes < closeMinutes) {
+  if (current >= openTime && current < closeTime) {
     return "OPEN";
   }
-
   return "CLOSED";
-};
+}
+
+module.exports = getStatus;
